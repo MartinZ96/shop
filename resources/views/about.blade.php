47acbbb736e51c9993a2b57023f456d73293c6f9
MartinZ96 <martynasss16@gmail.com>
2021-10-16 19:53:30 +0300

    FirstCommit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+
+@section('content')
+
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">About us</h1>
+                <p class="lead fw-normal text-white-50 mb-0">There is some things you should know about us.</p>
+            </div>
+        </div>
+    </header>
+    <!-- Section-->
+<div class="container-sm">
+    <div class="text-black">
+     <p class="mt-4 mb-4">
+        Nullam ac scelerisque ex. Ut tincidunt, nisi vitae elementum vehicula, lectus tortor accumsan dolor, 
+        vel pharetra mi urna non mauris. Ut eget convallis orci, vel pretium ex. 
+        Curabitur vestibulum iaculis est, vel pretium tellus laoreet ut. Morbi laoreet sapien eu metus vulputate mattis. 
+        Etiam volutpat mattis nisi id pretium. Etiam dignissim enim erat, nec pulvinar mi laoreet nec. 
+        Integer at iaculis orci. Nulla et magna ac justo scelerisque vulputate a eu est. Donec suscipit orci sapien, 
+        quis rhoncus ante imperdiet id. Donec bibendum ultrices dictum. 
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae sem ante.
+        Maecenas eu porta tellus, eu convallis lorem.</p>
+</div>
+</div>
+
+@endsection
